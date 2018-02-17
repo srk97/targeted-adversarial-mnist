57803fc6c248b50bce785eaa3a0af265ec7a3969
sid <srk97c@gmail.com>
2018-02-17 22:41:34 +0530

    Refactor code

--- adversary.py
+++ adversary.py
@@ -4,8 +4,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 from scipy.misc import imsave
 
-e = 0.01
-num_steps = 25
+EPSILON = 0.01
+SAMPLE_SIZE = 10
+META_GRAPH = './model/deepnn.meta'
+CKPT = './model'
 
 # write_jpeg Source: https://stackoverflow.com/a/40322153/4989649
 def write_jpeg(data, filepath):
@@ -28,7 +30,7 @@ def get_data(true_label,data_dir):
 	y_test = mnist.test.labels
 
 	indexes = (y_test==true_label)
-	x_test_sample = np.reshape(x_test[indexes][0:10],(10,784))
+	x_test_sample = np.reshape(x_test[indexes][0:SAMPLE_SIZE],(SAMPLE_SIZE,784))
 	return x_test_sample
 
 if __name__ == '__main__':
@@ -44,38 +46,36 @@ if __name__ == '__main__':
 	#Convert args to dictionary
 	arguments = args.__dict__
 
-	#Restore the model
-	sess = tf.Session()
-	saver = tf.train.import_meta_graph('./model/deepnn.meta')
-	saver.restore(sess,tf.train.latest_checkpoint('./model/'))
-	graph = tf.get_default_graph()	
-
 	x_feed = get_data(arguments['input_class'],arguments['data_dir'])
 
-	accuracy = []
 	x_adversarial = []
+
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		sess.run(tf.local_variables_initializer())
 		graph = tf.get_default_graph()
 
 		#Restore the model
-		saver = tf.train.import_meta_graph('./model/deepnn.meta')
-		saver.restore(sess, tf.train.latest_checkpoint('./model/'))	
+		saver = tf.train.import_meta_graph(META_GRAPH)
+		saver.restore(sess, tf.train.latest_checkpoint(CKPT))	
 
-		#Restore placeholders
+		#Restore placeholders and tensors
 		x = graph.get_tensor_by_name('x:0')
 		y = graph.get_tensor_by_name('y_:0')
 		keep_prob = graph.get_tensor_by_name('dropout/keep_prob:0')
 		cse_loss = graph.get_tensor_by_name('cross-entropy:0')		
+		acc = graph.get_tensor_by_name('acc:0')
 
-		for j in range(x_feed.shape[0]):
+		for j in range(SAMPLE_SIZE):
 			print "Creating adversarial image for image {}".format(j)
-			for i in range(num_steps):
+			feed_dict = {x:np.reshape(x_feed[j],(1,784)),y:np.reshape(arguments['target_class'],(1,)),keep_prob:1.0}
+
+			i=0
+			#Run the loop till the prediction is the target class
+			while sess.run(acc,feed_dict={x:np.reshape(x_feed[j],(1,784)),y:np.reshape(arguments['target_class'],(1,)),keep_prob:1.0})!=1.0:
 				print "\tStep: {}".format(i)
 
-				# Perapare the dictionary and get the gradients from J_{\theta}(x,y_target)
-				feed_dict = {x:np.reshape(x_feed[j],(1,784)),y:np.reshape(arguments['target_class'],(1,)),keep_prob:1.0}
+				#Get the gradients from J_{\theta}(x,y_target)
 				cse=sess.run(cse_loss,feed_dict=feed_dict)
 				grads = tf.gradients(cse_loss, x, grad_ys=None, name='gradients', colocate_gradients_with_ops=False, gate_gradients=False, aggregation_method=None)	
 				grads_ = sess.run(grads,feed_dict=feed_dict)
@@ -83,19 +83,16 @@ if __name__ == '__main__':
 				#Reshape gradients to match the input shape and update the image
 				grads_ = np.asarray(grads_)
 				grads_ = np.reshape(grads_,(1,784))
-				x_feed[j] = x_feed[j]-(e*(np.sign(grads_)))		
+				x_feed[j] = x_feed[j]-(EPSILON*(np.sign(grads_)))		
 				x_feed[j] = np.clip(x_feed[j], 0, 1.)
 
-			#Calculate accuracy w.r.t target class as a check post-perturbations	
-			acc = graph.get_tensor_by_name('acc:0')
-			accuracy.append(sess.run(acc,feed_dict={x:np.reshape(x_feed[j],(1,784)),y:np.reshape(arguments['target_class'],(1,)),keep_prob:1.0}))
-			x_adversarial.append(x_feed[j])
+				i+=1
 
-	for i in accuracy:
-		print i
+			x_adversarial.append(x_feed[j])
 
 	output=[]
 	x_original = get_data(arguments['input_class'],arguments['data_dir'])
+
 	for i in range(x_original.shape[0]):
 
 		adversarial = np.array(x_feed[i]).reshape(28, 28, 1)
@@ -108,6 +105,7 @@ if __name__ == '__main__':
 		out = np.array(out).reshape(28, 84, 1)
 		out = np.multiply(out,255)
 		output.append(out)
+
 	#10 rows as sample size	
-	out = np.array(output).reshape(28*10,84,1)
+	out = np.array(output).reshape(28*SAMPLE_SIZE,84,1)
 	write_jpeg(out, 'image.jpg')	
